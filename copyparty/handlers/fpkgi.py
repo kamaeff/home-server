688204a7ce26b04531d1e83c7425cdf56eb37512
Danila Kamaev <github@kamaeff.com>
2026-04-24 10:15:34 +0400

    copyparty fpkgi handler: support copyparty vfs and permissions

--- copyparty/handlers/fpkgi.py
+++ copyparty/handlers/fpkgi.py
@@ -3,9 +3,30 @@
 # from https://github.com/kamaeff/copyparty-dumb-fpkgi-handler/blob/master/fpkgi.py
 
 import json
-from pathlib import Path
-import re
+from collections import namedtuple
 from urllib.parse import quote
+import re
+
+
+permissions_fields = ('read', 'write', 'move', 'delete', 'get', 'upget', 'html', 'admin', 'dot')
+Permissions = namedtuple('Permissions', permissions_fields, defaults=(False,) * len(permissions_fields))
+
+# any of these is fine
+REQUIRED_PERMISSIONS = [
+    Permissions(read=True),
+    Permissions(get=True),
+    Permissions(upget=True),
+    Permissions(html=True),
+    Permissions(admin=True),
+]
+CUSA_PATTERN = re.compile(r"CUSA\d{5}")
+ANY_PATTERN = re.compile(r"[A-Z]{4}\d{5}")
+
+
+def extract_package_id(filename: str) -> str:
+    match = CUSA_PATTERN.search(filename) or ANY_PATTERN.search(filename)
+    return match[0] if match else 'UNKNOWN'
+
 
 
 def main(cli, vn, rem):
@@ -13,33 +34,28 @@ def main(cli, vn, rem):
         return cli.tx_404()
 
     protocol = "https" if cli.is_https else "http"
-    host = cli.host
-    local_path_prefix = vn.realpath
-    url_path_prefix = vn.vpath
     basic_auth=''
     if cli.uname != '*' or cli.pw:
         basic_auth = f'{cli.uname}:{cli.pw}@'
+    base_url = f"{protocol}://{basic_auth}{cli.host}/"
+
 
     response = {}
-    for path in Path(local_path_prefix).rglob("*.pkg"):
-        relative_url_path = path.relative_to(local_path_prefix).as_posix()
-        url_path = quote(f"{url_path_prefix}/{relative_url_path}")
-        url = f"{protocol}://{basic_auth}{host}/{url_path}"
-        id_match = re.search(r"CUSA\d{5}", path.stem, flags=re.IGNORECASE)
-        if id_match is not None:
-            id_ = id_match[0]
-        else:
-            id_ = "UNKNOWN"
-        response[url] = {
-            "title_id": id_,
-            "region": None,
-            "name": path.stem,
-            "version": None,
-            "release": None,
-            "size": path.stat().st_size,
-            "min_fw": None,
-            "cover_url": None,
-        }
+    for walk_result in vn.walk('', '', [], cli.uname, REQUIRED_PERMISSIONS, 0, False, False, True):
+        vfs_subdir = walk_result[2]
+        files = (f for f in walk_result[4] if f[0].endswith('.pkg'))
+        for file_name, stat in files:
+            url = base_url + quote(f'{vn.vpath}/{vfs_subdir}/{file_name}')
+            response[url] = {
+                "title_id": extract_package_id(file_name),
+                "region": None,
+                "name": file_name[:-4],
+                "version": None,
+                "release": None,
+                "size": stat.st_size,
+                "min_fw": None,
+                "cover_url": None,
+            }
 
     response_body = json.dumps({"DATA": response})
 
