758115fd08aa5fe9d729eca480d29b2d868207ce
Danila Kamaev <github@kamaeff.com>
2026-05-02 21:51:07 +0400

    updated fpkgi metadata handling

--- copyparty/handlers/fpkgi.py
+++ copyparty/handlers/fpkgi.py
@@ -2,124 +2,361 @@
 # sends a custom response instead of the usual 404
 # from https://github.com/kamaeff/copyparty-dumb-fpkgi-handler/blob/master/fpkgi.py
 
+from enum import Enum
+from io import BufferedReader
 import json
 from collections import namedtuple
 from pathlib import Path
 from urllib.parse import quote
 import re
+import struct
 
 
-permissions_fields = ('read', 'write', 'move', 'delete', 'get', 'upget', 'html', 'admin', 'dot')
-Permissions = namedtuple('Permissions', permissions_fields, defaults=(False,) * len(permissions_fields))
+###### Main logic ######
 
-# any of these is fine
-REQUIRED_PERMISSIONS = [
-    Permissions(read=True),
-    Permissions(get=True),
-    Permissions(upget=True),
-    Permissions(html=True),
-    Permissions(admin=True),
-]
-CUSA_PATTERN = re.compile(r"CUSA\d{5}")
-ANY_PATTERN = re.compile(r"[A-Z]{4}\d{5}")
-
-
-def extract_package_id(filename: str) -> str:
-    match = CUSA_PATTERN.search(filename) or ANY_PATTERN.search(filename)
-    return match[0] if match else 'UNKNOWN'
-
-
+COVER_VFS_PREFIX = '__fpkg_cover/'
+JSON_VFS_PATH = '__FPKGi.json'
 
 def main(cli, vn, rem):
-    if rem != '__FPKGi.json':
-        return cli.tx_404()
+    if rem == JSON_VFS_PATH:
+        return handle_json(cli, vn, rem)
+    elif rem.startswith(COVER_VFS_PREFIX):
+        return handle_cover(cli, vn, rem[len(COVER_VFS_PREFIX):])
+    return str(cli.tx_404())
 
+
+def handle_cover(cli, vn, rem):
+    vfs_path = Path(vn.vpath, rem)
+
+    if not REQUIRED_PERMISSIONS.can_access(vn, vfs_path, cli.uname):
+        return str(cli.tx_403())
+
+    with PkgFile(Path(vn.realpath, rem)) as pkg:
+        image = pkg.extract_cover_image()
+    if image is None:
+        return str(cli.tx_404())
+    return str(cli.reply(image, 200, "image/png"))
+
+
+def handle_json(cli, vn, rem):
     protocol = "https" if cli.is_https else "http"
     basic_auth=''
     if cli.uname != '*' or cli.pw:
         basic_auth = f'{cli.uname}:{cli.pw}@'
     base_url = f"{protocol}://{basic_auth}{cli.host}/"
 
-
     response = {}
-    for walk_result in vn.walk('', '', [], cli.uname, REQUIRED_PERMISSIONS, 0, False, False, True):
+    for walk_result in vn.walk('', '', [], cli.uname, REQUIRED_PERMISSIONS.permissions, 0, False, False, True):
         vfs_subdir = walk_result[2]
         fs_parent_dir = walk_result[3]
         files = (f for f in walk_result[4] if f[0].endswith('.pkg'))
         for file_name, stat in files:
+            with PkgFile(Path(fs_parent_dir, file_name)) as pkg:
+                param_sfo = pkg.extract_param_sfo()
+                has_cover_image = pkg.has_cover_image()
+
             url = base_url + quote(f'{vn.vpath}/{vfs_subdir}/{file_name}')
-            param_sfo = extract_param_sfo(Path(fs_parent_dir, file_name))
-            title_id = param_sfo.get('TITLE_ID') or extract_package_id(file_name)
-            content_id = param_sfo.get('CONTENT_ID')
-            response[url] = {
-                "title_id": title_id,
-                "region": REGIONS.get(content_id[0].upper()) if content_id else None,
-                "name": param_sfo.get('TITLE') or file_name[:-4],
-                "version": param_sfo.get('VERSION'),
-                "release": None,
-                "size": stat.st_size,
-                "min_fw": param_sfo.get('SYSTEM_VER'),
-                "cover_url": None,
-            }
+            icon_url = (base_url + quote(f'{vn.vpath}/{COVER_VFS_PREFIX}{vfs_subdir}/{file_name}')) if has_cover_image else None
+            response[url] = format_pkg_params(param_sfo, file_name[:-4], icon_url, stat.st_size)
 
-    response_body = json.dumps({"DATA": response})
+    response_body = json.dumps({"DATA": response}).encode("utf-8")
 
-    return str(cli.reply(response_body.encode("utf-8"), 200, "application/json"))
+    return str(cli.reply(response_body, 200, "application/json"))
+
+###### /Main logic ######
 
 
+###### PKG to FPKGi data conversion ######
 
-
-### FPKG PARAM.SFO ###
-
-from enum import Enum
-import struct
-
-
-PARAM_SFO_ENTRY_ID = 0x00001000
-REQUIRED_PARAMS = {'TITLE_ID', 'TITLE', 'CONTENT_ID', 'VERSION', 'SYSTEM_VER', 'CATEGORY'}
 REGIONS = {
     'U': 'USA',
     'J': 'JAP',
     'E': 'EUR',
     'H': 'ASIA',
-
     # 'I': 'INT',
     # 'K': 'KOREA'
     'K': 'ASIA'
 }
-
-class SfoParamType(Enum):
-    UTF8_NULL_TERMINATED = 0x4
-    UTF8_FIXED_LENGTH = 0x204
-    INT = 0x404
+# CUSA is most common, catch it first
+TITLE_ID_PATTERN = re.compile(r"(CUSA|[A-Z]{4})\d{5}")
 
 
-class BinaryFile(object):
-    def __init__(self, file):
-        self.file = file
+def format_pkg_params(param_sfo: dict, file_name: str, cover_url: str, file_size: int):
+    if param_sfo is None:
+        return {
+            'cover_url': None,
+            'release': None,
+            'size': file_size,
+            'min_fw': None,
+            'title_id': 'UNKNOWN',
+            'region': None,
+            'version': None,
+            'category': 'homebrew',
+            'name': f'BROKEN PKG FILE | {file_name}'
+        }
+
+    response = {
+        'cover_url': cover_url,
+        'release': None,
+        'size': file_size,
+        'min_fw': param_sfo.get('SYSTEM_VER'),
+    }
+
+    title_id = param_sfo.get('TITLE_ID')
+    if title_id is None:
+        match = TITLE_ID_PATTERN.search(file_name)
+        title_id = match[0] if match else 'UNKNOWN'
+    response['title_id'] = title_id
+
+    content_id = param_sfo.get('CONTENT_ID')
+    response['region'] = REGIONS.get(content_id[0].upper()) if content_id else None
+
+    versions = []
+    for param_name in 'APP_VER', 'VERSION', 'CONTENT_VER':
+        version = param_sfo.get(param_name)
+        if version:
+            versions.append(f'{param_name[0]}{version}')
+    response['version'] = '_'.join(versions) or None
+
+    category = Category(param_sfo.get('CATEGORY'), title_id, file_name)
+    response['category'] = category.fpkgi_category
+
+    response['name'] = f'{param_sfo.get('TITLE') or 'BROKEN PKG FILE'} [{category.title_prefix}] | {file_name}'
+
+    return response
+
+
+class Category(object):
+    _MAPPING = {
+        'ac': 'DLC',
+        'bd': 'games',
+        'gc': 'games',
+        'gd': 'games',
+        'gda': 'apps',
+        'gdb': 'apps',
+        'gdc': 'apps',
+        'gdd': 'apps',
+        'gde': 'apps',
+        'gdg': 'apps',
+        'gdk': 'apps',
+        'gdl': 'apps',
+        'gdo': 'PS2',
+        'gdO': 'PS2',
+        'gd0': 'PS2',
+        'gp': 'updates',
+        'gpc': 'updates',
+        'gpd': 'updates',
+        'gpe': 'updates',
+        'gpk': 'updates',
+        'gpl': 'updates',
+    }
+    _TITLE_PREFIX = {
+        'DLC': 'DLC',
+        'games': 'Game',
+        'apps': 'App',
+        'PS2': 'PS2',
+        'updates': 'Upd',
+        'homebrew': 'HB'
+    }
+    _PS2_PATTERN = re.compile(r"S[CL][PUE][SMD]")
+    _BACKPORT_FILENAME_PATTERN = re.compile(r'BACKPORT|FIX[4567]|(?<![A-Z])BP(?![A-Z])|CYB1K', re.IGNORECASE)
+
+    def __init__(self, sfo_category, title_id, file_name):
+        self.sfo_category = sfo_category
+
+        if self._PS2_PATTERN.match(title_id):
+            self.fpkgi_category = 'PS2'
+        else:
+            self.fpkgi_category = self._MAPPING.get(sfo_category, 'homebrew')
+
+        if self._BACKPORT_FILENAME_PATTERN.search(file_name):
+            self.title_prefix = 'BP'
+        else:
+            self.title_prefix = self._TITLE_PREFIX.get(self.fpkgi_category, self.fpkgi_category)
+
+###### /PKG to FPKGi data conversion ######
+
+
+###### Copyparty stuff ######
+
+permission_fields = ('read', 'write', 'move', 'delete', 'get', 'upget', 'html', 'admin', 'dot')
+Permission = namedtuple('Permission', permission_fields, defaults=(False,) * len(permission_fields))
+
+class PermSet(object):
+    def __init__(self, *permissions):
+        self.permissions = permissions
+
+    def check(self, requested_permission: Permission):
+        """Checks if provided permission matches this PermSet"""
+        for required_permission in self.permissions:
+            for required, existing in zip(required_permission, requested_permission):
+                # found mismatching option in currently checked required permission
+                if required and not existing: break
+            else:
+                # no mismatching options at least for one permission in this PermSet
+                return True
+        return False
+
+    def can_access(self, vfs, path, username):
+        """Checks if spcified user can access specified path in specified VFS"""
+        existing_permission = vfs.can_access(str(path), uname=username)
+        return self.check(existing_permission)
+
+
+# any of r/g/u/h/a is fine
+REQUIRED_PERMISSIONS = PermSet(
+    Permission(read=True),
+    Permission(get=True),
+    Permission(upget=True),
+    Permission(html=True),
+    Permission(admin=True),
+)
+
+###### /Copyparty stuff ######
+
+
+###### FPKG stuff ######
+
+class PkgFile(object):
+    ENTRY_ID_PARAM_SFO = 0x1000
+    ENTRY_ID_ICON0_PNG = 0x1200
+    ENTRY_ID_PIC0_PNG = 0x1220
+
+    SFO_TYPE_INT = 0x404
+    SFO_TYPE_UTF8_NULL_TERMINATED = 0x204
+    SFO_TYPE_UTF8_NO_NULL = 0x04
+
+    REQUIRED_PARAMS = {'TITLE_ID', 'TITLE', 'CONTENT_ID', 'VERSION', 'APP_VER', 'SYSTEM_VER', 'CATEGORY', 'EMU_VERSION', 'SYSTEM_ROOT_VER', 'CONTENT_VER', 'PUBTOOLINFO'}
+
+    def __init__(self, filepath: str | Path):
+        try:
+            self.file = open(filepath, 'rb')
+            # check PKG file magic
+            if self.seek(0).read_uint_be() != 0x7F434E54:
+                raise Exception(f'Invalid PKG magic for file {filepath}')
+            self.entries_locations = self._locate_entries()
+            self.is_valid = True
+        except Exception as e:
+            print(e)
+            self.close()
+            self.is_valid = False
+            self.entries_locations = {}
+
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        return self.file.__exit__(*args, **kwargs)
+
+    def close(self, *args, **kwargs):
+        if hasattr(self, 'file') and self.file is not None:
+            return self.file.close(*args, **kwargs)
+
+    def _locate_entries(self):
+        # read pkg header to get to metadata entries
+        entry_count = self.seek(0x10).read_uint_be()
+        entry_table_position = self.seek(0x18).read_uint_be()
+
+        entries_locations = {}
+        self.seek(entry_table_position)
+        for _ in range(entry_count):
+            entry_id = self.read_uint_be()
+            entries_locations[entry_id] = self.seek(12,1).read_struct('>II')
+            self.seek(8,1)
+        return entries_locations
+
+    def has_cover_image(self):
+        return (
+            self.ENTRY_ID_ICON0_PNG in self.entries_locations
+            or
+            self.ENTRY_ID_PIC0_PNG in self.entries_locations
+        )
+
+    def extract_cover_image(self):
+        location = (
+            self.entries_locations.get(self.ENTRY_ID_ICON0_PNG)
+            or
+            self.entries_locations.get(self.ENTRY_ID_PIC0_PNG)
+        )
+        if location is None:
+            return None
+        return self.seek(location[0]).read(location[1])
+
+    def extract_param_sfo(self):
+        location = self.entries_locations.get(self.ENTRY_ID_PARAM_SFO)
+        if location is None:
+            return None
+
+        param_sfo_offset = location[0]
+        self.seek(param_sfo_offset)
+
+        # check SFO magic
+        if self.read_uint_be() == 0x53434543:
+            param_sfo_offset += 0x800
+
+        if self.seek(param_sfo_offset).read_uint_be() != 0x00505346:
+            return None
+
+        params = dict.fromkeys(self.REQUIRED_PARAMS, None)
+
+        # obtain params info
+        key_table_offset = self.seek(param_sfo_offset + 8).read_int_le()
+        data_table_offset = self.read_int_le()
+        values_count = self.read_int_le()
+
+        for idx in range(values_count):
+            # get param entry info
+            self.seek(idx * 0x10 + 0x14 + param_sfo_offset)
+            key_offset, format_, length, _, data_offset = self.read_struct('<HHiiI')
+
+            # get param name
+            self.seek(param_sfo_offset + key_table_offset + key_offset)
+            name = self.read_ascii()
+            if name not in self.REQUIRED_PARAMS:
+                continue
+
+            # get param value
+            self.seek(param_sfo_offset + data_table_offset + data_offset)
+            if format_ == self.SFO_TYPE_INT:
+                if name == 'SYSTEM_VER':
+                    _, _, minor, major = self.read_struct('4b')
+                    value = f'{major:x}.{minor:02x}'
+                else:
+                    value = self.read_int_le()
+            elif format_ == self.SFO_TYPE_UTF8_NULL_TERMINATED:
+                value = self.read_utf8(length - 1)
+            elif format_ == self.SFO_TYPE_UTF8_NO_NULL:
+                value = self.read_utf8(length)
+            else:
+                value = None
+
+            params[name] = value
+
+        return params
 
     def seek(self, offset, whence=0, /):
         self.file.seek(offset, whence)
         return self
-    
+
     def read_struct(self, format):
         size = struct.calcsize(format)
         return struct.unpack(format, self.file.read(size))
 
-    def uint_be(self):
+    def read_uint_be(self):
         return self.read_struct('>I')[0]
 
-    def int_le(self):
+    def read_int_le(self):
         return self.read_struct('<i')[0]
 
-    def uint_le(self):
+    def read_uint_le(self):
         return self.read_struct('<I')[0]
 
-    def ushort_le(self):
+    def read_ushort_le(self):
         return self.read_struct('<H')[0]
 
     # no need to sacrifice readability I guess
-    def ascii(self):
+    def read_ascii(self):
         res = bytearray()
         while True:
             byte = self.file.read(1)[0]
@@ -127,94 +364,14 @@ class BinaryFile(object):
                 return res.decode('ASCII')
             res.append(byte)
 
-    def utf8(self, length):
-        length = length - 1 if length > 0 else 0
-        if length > 1:
-            return self.file.read(length - 1).decode('utf-8')
+    def read_utf8(self, length):
+        if length > 0:
+            return self.file.read(length).decode('utf-8')
         return None
 
     def __getattr__(self, name):
         return getattr(self.file, name)
 
-
-def extract_param_sfo(filepath: str):
-    with open(filepath, 'rb') as f:
-        file = BinaryFile(f)
-        param_sfo_location = find_param_sfo_location(file)
-        if param_sfo_location is None:
-            return create_empty_params()
-        param_sfo = collect_sfo_params(file, param_sfo_location)
-    return(param_sfo)
+###### /FPKG stuff ######
 
 
-def find_param_sfo_location(file: BinaryFile):
-    # read pkg header to get pkg entries details
-    entry_count = file.seek(0x10).uint_be()
-    entry_table_position = file.seek(0x18).uint_be()
-
-    # look for param.sfo metadata entry in metadata entries
-    file.seek(entry_table_position)
-    for _ in range(entry_count):
-        if file.uint_be() == PARAM_SFO_ENTRY_ID:
-            break
-        file.seek(28, 1)
-    else:
-        return None
-
-    # extract actual param.sfo entry location
-    return file.seek(12, 1).uint_be()
-
-
-def collect_sfo_params(file: BinaryFile, param_sfo_start: int):
-    file.seek(param_sfo_start)
-
-    # check SFO magic
-    if file.uint_be() == 0x53434543:
-        param_sfo_start += 0x800
-
-    if file.seek(param_sfo_start).uint_be() != 0x00505346:
-        return None
-
-    params = create_empty_params()
-
-    # obtain params info
-    key_table_offset = file.seek(param_sfo_start + 8).int_le()
-    data_table_offset = file.int_le()
-    values_count = file.int_le()
-
-    for idx in range(values_count):
-        key_offset = file.seek(idx * 0x10 + 0x14 + param_sfo_start).ushort_le()
-        format_ = file.ushort_le()
-        length = file.int_le()
-        file.seek(4, 1)
-        data_offset = file.uint_le()
-
-        try:
-            name = file.seek(param_sfo_start + key_table_offset + key_offset).ascii()
-        except:
-            continue
-
-        if name not in REQUIRED_PARAMS:
-            continue
-
-        file.seek(param_sfo_start + data_table_offset + data_offset)
-
-        if format_ == SfoParamType.INT.value:
-            if name == 'SYSTEM_VER':
-                _, _, minor, major = file.read_struct('4b')
-                value = f'{major:x}.{minor:02x}'
-            else:
-                value = file.int_le()
-        elif format_ == SfoParamType.UTF8_FIXED_LENGTH.value:
-            value = file.utf8(length + 1)
-        elif format_ == SfoParamType.UTF8_NULL_TERMINATED.value:
-            value = file.utf8(length)
-        else:
-            value = None
-
-        params[name] = value
-
-    return params
-
-def create_empty_params():
-    return dict.fromkeys(REQUIRED_PARAMS, None)
