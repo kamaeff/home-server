3f3b346d036059af827d70350afa761581b51c97
Danila Kamaev <github@kamaeff.com>
2026-07-27 16:49:57 +0400

    added ps4 webkit jailbreak

--- caddy/build-static.sh
+++ caddy/build-static.sh
@@ -5,5 +5,12 @@ set -e
 rm -rf /static/*
 
 wget -O /tmp/aria-ng.zip https://github.com/mayswind/AriaNg/releases/download/1.3.13/AriaNg-1.3.13.zip
+unzip /tmp/aria-ng.zip -d /static/aria-ng-tmp
+# only do atomic mv on success of previous operations
+mv /static/aria-ng-tmp /static/aria-ng
 
-unzip /tmp/aria-ng.zip -d /static/aria-ng
+
+git clone --depth=1 https://github.com/kamaeff/ps4-jb-webkit.git /tmp/ps4-jb-webkit
+mv /tmp/ps4-jb-webkit/public /static/ps4-jb-webkit-tmp
+# only do atomic mv on success of previous operations
+mv /static/ps4-jb-webkit-tmp /static/ps4-jb-webkit
